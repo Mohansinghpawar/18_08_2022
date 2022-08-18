6381990fd3a7f5d23c8d5ca350080c1cb597fb65
Mohansinghpawar <88230569+Mohansinghpawar@users.noreply.github.com>
2022-08-18 10:34:06 +0530

    Create ObjectDetails.js

---
+++ ObjectDetails.js
@@ -0,0 +1,8 @@
+let CandidateTrainee={
+    name: "mohan",
+    age: 22,
+    doj: "08 aug"
+}
+console.log(CandidateTrainee)
+var name="Mohan Singh Pawar"
+console.log("Welcome to Clayfin!!!"+" "+name)
